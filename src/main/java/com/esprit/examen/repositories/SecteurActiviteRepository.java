dc79f2c73de3704f1e66e337b1fd6403603f8bd2
nourhenekheriji <nourhene.kheriji@esprit.tn>
2022-11-06 19:01:57 +0100

    first commit

--- src/main/java/com/esprit/examen/repositories/SecteurActiviteRepository.java
+++ src/main/java/com/esprit/examen/repositories/SecteurActiviteRepository.java
@@ -1,6 +1,6 @@
 package com.esprit.examen.repositories;
 
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
